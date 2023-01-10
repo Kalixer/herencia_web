1f2422d377392ac678ee7e8d4ad22bd54bf62012
Kalixer <al.caceres.ag72@gmail.com>
2023-01-09 22:19:33 -0300

    Changes on the size and distribution of the 'protafolio' section. White shadows on the 'main-message'

--- src/pages/Temp.jsx
+++ src/pages/Temp.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import '@styles/Home.scss';
-import logo from '@icons/logo.jpg';
+import logo from '@icons/logo.jpg'; // It's a logo
+
+// Colored works
 import atardecer from '@pics/atardecer.jpg';
 import zorro from '@pics/zorro.jpg';
 import red_hair_girl from '@pics/red_hair_girl.jpg';
+import ferrari from '@pics/ferrari.jpg'
+
+// Graphite
 import caballo from '@pics/caballo.jpg';
 import ardilla from '@pics/ardilla.jpeg';
 import puppy_paw from '@pics/puppy_paw.jpg';
+import auto_antiguo from '@pics/auto_antiguo.jpg';
 
 const trabajos_color = [
     <figure className="port-box">
@@ -17,6 +23,9 @@ const trabajos_color = [
     </figure>,
     <figure className="port-box">
         <img src={atardecer} alt=""/>
+    </figure>,
+    <figure className="port-box">
+        <img src={ferrari} alt=""/>
     </figure>,
 ]
 const trabajos_grafito = [
@@ -29,20 +38,15 @@ const trabajos_grafito = [
     <figure className="port-box">
         <img src={caballo} alt=""/>
     </figure>,
+    <figure className="port-box">
+        <img src={auto_antiguo} alt=""/>
+    </figure>,
 ]
 
 let window_width = window.innerWidth; //Ancho de la pantalla
 
 
 const Temp = () => {
-
-    if(window_width < 1455){
-        trabajos_color.length = 2
-        trabajos_grafito.length = 2
-        console.log(trabajos_color)
-        console.log(trabajos_grafito)
-    }
-
     return (
         <div>
             <nav>
@@ -68,13 +72,16 @@ const Temp = () => {
             <h2>Regala, decora... O aprende a hacer arte</h2>
         </div>
         <div className="portfolio">
-            <h2>Conoce nuestros trabajos</h2>
             <div className="port-draws">
+            <h2>Conoce nuestros trabajos</h2>
 
                 <div className="section-box">
                     <p>Color</p>
                     <div className="subsection-box">
-                        {trabajos_color}
+                        {trabajos_color[0]}
+                        {trabajos_color[1]}
+                        {trabajos_color[2]}
+                        {trabajos_color[3]}
                     </div>
                     <a href="#">Ver más &#8594;</a>
                 </div>
@@ -82,7 +89,10 @@ const Temp = () => {
                 <div className="section-box">
                     <p>Grafito</p>
                     <div className="subsection-box">
-                        {trabajos_grafito}
+                        {trabajos_grafito[0]}
+                        {trabajos_grafito[1]}
+                        {trabajos_grafito[2]}
+                        {trabajos_grafito[3]}
                     </div>
                     <a href="#">Ver más &#8594;</a>
                 </div>
